b86d0f8f6a0ba4f972a56ddbb267faf78e78bb34
NingF324 <2283451158@qq.com>
2022-12-22 17:50:16 +0800

    1

--- 函数模板.cpp
+++ 函数模板.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-template<class T>
+template <class T>
 class Stack
 {
 private:
@@ -11,39 +11,40 @@ public:
 	Stack(int = 10);
 	~Stack()
 	{
-		delete[]space;
+		delete[] space;
 	}
 	bool push(const T&);
-	T pop();
-	bool IsEmpty()const
+	T  pop();
+	bool IsEmpty() const
 	{
 		return top == size;
 	}
-	bool IsFull()const
+	bool IsFull() const
 	{
 		return top == 0;
 	}
 };
 
-template<class T>
-Stack<T>::Stack(int size)
+
+template <class T>
+Stack <T>::Stack(int size)
 {
 	this->size = size;
 	space = new T[size];
 	top = size;
 }
-template<class T>
-bool Stack<T>::push(const T& element)
+template <class T>
+bool Stack <T>::push(const T& element)
 {
 	if (!IsFull())
 	{
-		space[--top] == element;
+		space[--top] = element;
 		return true;
 	}
 	return false;
 }
-template<class T>
-T Stack<T>::pop()
+template <class T>
+T  Stack <T>::pop()
 {
 	return space[top++];
 }
@@ -56,10 +57,10 @@ int main()
 	S1.push('u');
 	S1.push('v');
 	while (!S1.IsEmpty())
-	cout << S1.pop() << endl;
+		cout << S1.pop() << endl;
 	return 0;
-
 }
+
 
 /*template<class T_ID, class T_Score, int num>
 class Student {
